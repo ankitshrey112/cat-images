333488efbf1bf7d63ea2aa13165c76d5d72f8291
Ankit Shrey <ankit.shrey@cogoport.com>
2023-09-27 03:42:10 +0530

    add list api spec with custom pagination

--- spec/services/list_cat_images_spec.rb
+++ spec/services/list_cat_images_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe ListCatImages do
         CatImage.create!(cat_image)
       end.reverse
 
-      it 'returns a list of cat images with pagination data' do
+      it 'returns a list of cat images with default pagination data' do
         result = described_class.run()
 
         expect(result).to be_valid
@@ -100,6 +100,36 @@ RSpec.describe ListCatImages do
         expect(data).to have_key(:current_page) and expect(data[:current_page]).to eql(1)
         expect(data).to have_key(:current_count) and expect(data[:current_count]).to eql(10)
         expect(data).to have_key(:total_pages) and expect(data[:total_pages]).to eql(count/10 + (count%10 == 0 ? 0 : 1))
+        expect(data).to have_key(:total_count) and expect(data[:total_count]).to eql(count)
+
+        list = data[:list]
+
+        cat_images_objects.each_with_index do |cat_image, index|
+          list_obj = list[index]
+          expect(list_obj).not_to be nil
+
+          expect(list_obj).to have_key(:id) and expect(list_obj[:id]).to eql(cat_image.id)
+          expect(list_obj).to have_key(:name) and expect(list_obj[:name]).to eql(cat_image.name)
+          expect(list_obj).to have_key(:age) and expect(list_obj[:age]).to eql(cat_image.age)
+          expect(list_obj).to have_key(:breed) and expect(list_obj[:breed]).to eql(cat_image.breed)
+          expect(list_obj).to have_key(:image_url) and expect(list_obj[:image_url]).not_to be_empty
+          expect(list_obj).to have_key(:created_at) and expect(list_obj[:created_at].to_s).not_to be_empty
+        end
+      end
+
+      it 'returns a list of cat images with custom pagination data' do
+        result = described_class.run({page: 2, page_limit: 3})
+
+        expect(result).to be_valid
+        expect(result.errors).to be_empty
+
+        data = result.result
+        count = CatImage.where(status: CatImageStatus::ACTIVE).count
+
+        expect(data).to have_key(:list)
+        expect(data).to have_key(:current_page) and expect(data[:current_page]).to eql(2)
+        expect(data).to have_key(:current_count) and expect(data[:current_count]).to eql(3)
+        expect(data).to have_key(:total_pages) and expect(data[:total_pages]).to eql(count/3 + (count%3 == 0 ? 0 : 1))
         expect(data).to have_key(:total_count) and expect(data[:total_count]).to eql(count)
 
         list = data[:list]
